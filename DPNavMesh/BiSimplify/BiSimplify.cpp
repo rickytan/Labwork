b428c31b371c8810191f735249c371698b9f099f
rickytan <ricky.tan.xin@gmail.com>
2014-02-21 20:01:17 +0800

    new project

---
+++ DPNavMesh/BiSimplify/BiSimplify.cpp
@@ -0,0 +1,44 @@
+// BiSimplify.cpp : 定义控制台应用程序的入口点。
+//
+
+#pragma warning(disable:4819)
+
+#include "stdafx.h"
+#include "mesh.h"
+
+
+int main(int argc, char* argv[])
+{
+	Mesh mesh;
+
+	char *mesh_file	= "bunny_closed.obj";
+	char *out_file	= "bunny_out.obj";
+	int final_size	= 1000;
+
+	printf("Loading %s\n", mesh_file);
+	int error = vcg::tri::io::Importer<Mesh>::Open(mesh, mesh_file);
+	if(error)
+	{
+		printf("Unable to open mesh %s : '%s'\n", mesh_file, vcg::tri::io::Importer<Mesh>::ErrorMsg(error));
+		exit(-1);
+	}
+	printf("Mesh loaded with Vertex: %d\tFace: %d", mesh.vn, mesh.fn);
+
+	vcg::tri::TriEdgeCollapseQuadricParameter params;
+	params.QualityThr = .3;
+
+	vcg::tri::UpdateBounding<Mesh>::Box(mesh);
+
+	vcg::LocalOptimization<Mesh> optim(mesh, &params);
+	optim.Init<TriEdgeCollapse>();
+	optim.SetTargetSimplices(final_size);
+	optim.SetTimeBudget(0.5f);
+
+	while (optim.DoOptimization() && mesh.fn > final_size)
+		printf("Mesh size %7i \r", mesh.fn);
+
+	vcg::tri::io::Exporter<Mesh>::Save(mesh, out_file);
+
+	return 0;
+}
+
---
+++ DPNavMesh/BiSimplify/mesh.h
@@ -0,0 +1,91 @@
+
+#ifndef __MESH_H_
+#define __MESH_H_
+
+#pragma warning(disable:4996)
+#pragma warning(disable:4244) // double to float
+
+
+#include <vector>
+#include <limits>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// stuff to define the mesh
+#include <vcg/complex/complex.h>
+#include <vcg/simplex/face/base.h>
+#include <vcg/simplex/edge/base.h>
+#include <vcg/complex/complex.h>
+
+#include <vcg/math/quadric.h>
+#include <vcg/complex/algorithms/clean.h>
+
+// io
+#include <wrap/io_trimesh/import.h>
+#include <wrap/io_trimesh/export.h>
+
+// update
+#include <vcg/complex/algorithms/update/topology.h>
+
+// local optimization
+#include <vcg/complex/algorithms/local_optimization.h>
+#include <vcg/complex/algorithms/local_optimization/tri_edge_collapse_quadric.h>
+
+
+class Vertex;
+class Edge;
+class Face;
+
+struct UsedTypes: public vcg::UsedTypes<
+	vcg::Use<Vertex>::AsVertexType,
+	vcg::Use<Edge>::AsEdgeType,
+	vcg::Use<Face>::AsFaceType
+>{};
+
+class Vertex: public vcg::Vertex<
+	UsedTypes,
+	vcg::vertex::VFAdj,
+	vcg::vertex::Coord3f,
+	vcg::vertex::Normal3f,
+	vcg::vertex::Mark,
+	vcg::vertex::BitFlags
+> {
+public:
+	vcg::math::Quadric<double> &Qd() {return q;}
+
+private:
+	vcg::math::Quadric<double> q;
+};
+
+class Edge: public vcg::Edge<UsedTypes> {};
+
+class Face: public vcg::Face<
+	UsedTypes,
+	vcg::face::VFAdj,
+	vcg::face::VertexRef,
+	vcg::face::BitFlags
+> {};
+
+typedef vcg::tri::BasicVertexPair<Vertex> VertexPair;
+
+class Mesh: public vcg::tri::TriMesh<std::vector<Vertex>, std::vector<Face>> {};
+
+class TriEdgeCollapse: public vcg::tri::TriEdgeCollapseQuadric<
+	Mesh,
+	VertexPair,
+	TriEdgeCollapse,
+	vcg::tri::QInfoStandard<Vertex>
+> {
+public:
+	TriEdgeCollapse(const VertexPair &p, int i, vcg::BaseParameterClass *pp): vcg::tri::TriEdgeCollapseQuadric<
+		Mesh,
+		VertexPair,
+		TriEdgeCollapse,
+		vcg::tri::QInfoStandard<Vertex>
+		> (p, i, pp) {}
+};
+
+
+
+#endif
