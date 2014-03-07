3ec24f3b5f333f498115e91c7fbdaf949666fb78
rickytan <ricky.tan.xin@gmail.com>
2014-03-07 15:46:05 +0800

    roll back to vs2008

--- DPNavMesh/BiSimplify/BiSimplify.cpp
+++ DPNavMesh/BiSimplify/BiSimplify.cpp
@@ -2,7 +2,24 @@
 //
 
 #pragma warning(disable:4819)
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 #include "mesh.h"
+//#include "bi_optimization.h"
+
+#include <gl/glut.h>
+
+#ifndef MIN
+#define MIN(x, y) ((x)<(y)?(x):(y))
+#endif
+
+static GLint win_width = 800, win_height = 600;
+static GLfloat fovy = 45.0f;
+static GLfloat near = 0.1f, far = 500.0f;
+
+Mesh mesh;
 
 void Simplify(
 	__in const Mesh &mesh_in,
@@ -10,14 +27,83 @@ void Simplify(
 	__in const int final_face = 1000,
 	__in const float quality = 0.3f)
 {
+	
 
+}
 
+void init()
+{
+	glDisable(GL_LIGHTING);
+	glEnable(GL_CULL_FACE);
+	
+}
+
+void display()
+{
+	glClearColor(0, 0, 0, 0);
+	glClearDepth(1.0);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glColor3f(1.0, 1.0, 1.0);
+	glutWireTeapot(1.0);
+
+	gluLookAt(
+		0, 0, 10,
+		0, 0, 0, 
+		0, 1, 0);
+	glutSwapBuffers();
+}
+
+void idle()
+{
+	glutPostRedisplay();
+}
+
+void reshape(int width, int height)
+{
+	if (width != win_width || height != win_height) {
+		win_height = height;
+		win_width = width;
+
+		glutReshapeWindow(width, height);
+
+		GLfloat ratio = 4.0 / 2;
+		if (height > 0)
+			ratio = (GLfloat)width / height;
+
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		gluPerspective(fovy, ratio, near, far);
+
+		glMatrixMode(GL_MODELVIEW);
+		glViewport(0, 0, width, height);
+
+	}
+}
+
+void gui(int *argc, char *argv[])
+{
+	glutInit(argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	init();
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(win_width, win_height);
+	glutCreateWindow("Show");
+	
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutIdleFunc(idle);
+	
+	glutShowWindow();
+	glutMainLoop();
 }
 
 int main(int argc, char* argv[])
 {
-	Mesh mesh;
-
 	char *mesh_file	= "bunny_closed.obj";
 	char *out_file	= "bunny_out.obj";
 	int final_size	= 100;
